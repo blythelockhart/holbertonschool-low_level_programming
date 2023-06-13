711b44f1119780320cbc2b0dd80e084367e00809
blythelockhart <blythe.lockhart@att.net>
2023-06-12 19:42:44 -0700

    More pointers_arrays_strings commit

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - Write a function that concatenates two strings.
+ * @dest: Concatenated string
+ * @src: String to be concatenated
+ *
+ * Return: The dest to the concateneated strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int lengthD, lengthS;
+
+	lenD = 0;
+	lenS = 0;
+
+	while (*(dest + lenD) != '\0')
+	{
+		lenD++;
+	}
+	while (*(src + lenS) != '\0' && lenD < 97)
+	{
+		*(dest + lenD) = *(src + lenS);
+		lenD++;
+		lenS++;
+	}
+	*(dest + lenD) = '\0';
+	return (dest);
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -12,3 +12,11 @@ This is the readme for pointers_arrays_strings.
 9-strcpy.c
 100-atoi.c
 main.h
+0-strcat.c
+1-strncat.c
+2-strncpy.c
+3-strcmp.c
+4-rev_array.c
+5-string_toupper.c
+6-cap_string.c
+7-leet.c
