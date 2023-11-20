2065fc7b33e31ae5f774dc8100a31c1cbaad065e
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-20 09:55:30 -0800

    coded files 6-8

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,57 +1,33 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/**
- * insert_dnodeint_at_index - Inserts a new node at a given position.
- * @h: Pointer to a pointer to the head of the list.
- * @idx: Index of the list where the new node should be added.
- * @n: Value to be assigned to the new node.
- * Return: The address of the new node, or NULL if it failed.
- */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp;
+	dlistint_t *new_node, *temp = *h;
 	unsigned int i;
-	new_node = malloc(sizeof(dlistint_t));
 
-	if (new_node == NULL)
+	if (!(new_node = malloc(sizeof(dlistint_t))))
 		return (NULL);
 
 	new_node->n = n;
-
 	if (idx == 0)
 	{
-		new_node->prev = NULL;
-		new_node->next = *h;
-
-		if (*h != NULL)
+		new_node->prev = NULL, new_node->next = *h;
+		if (*h)
 			(*h)->prev = new_node;
-
-		*h = new_node;
-		return (new_node);
+		return *h = new_node;
 	}
 
-	/* Traverse the list to find the node at index (idx - 1) */
-	temp = *h;
-	for (i = 0; i < idx - 1 && temp != NULL; i++)
-	{
-		temp = temp->next;
-	}
+	for (i = 0; i < idx - 1 && temp; i++, temp = temp->next);
 
-	/* Check if it's possible to add the new node at the given index */
-	if (temp == NULL)
+	if (!temp)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
-	/* Update pointers to insert the new node at the specified index */
 	new_node->prev = temp;
 	new_node->next = temp->next;
-
-	if (temp->next != NULL)
-		temp->next->prev = new_node;
-
+	if (temp->next) temp->next->prev = new_node;
 	temp->next = new_node;
 
 	return (new_node);
