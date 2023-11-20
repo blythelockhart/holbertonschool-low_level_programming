ba05648f3fb43dc023ed18ea573947498e2329f7
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-20 10:24:06 -0800

    corrected files 0 and 1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
+	hash_table_t *new_table = NULL;
 	unsigned long int i;
 
 	if (size == 0)
@@ -15,12 +15,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	/* Allocate memory for the hash table structure */
 	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
+	if (!new_table)
 		return (NULL);
 
 	/* Allocate memory for the array of hash nodes */
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table->array == NULL)
+	new_table->array = malloc(sizeof(hash_node_t **) * size);
+	if (!new_table->array)
 	{
 		free(new_table);
 		return (NULL);
