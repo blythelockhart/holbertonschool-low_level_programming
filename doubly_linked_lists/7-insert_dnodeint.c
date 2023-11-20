af46e747833ff79ae0c26fc02ce1f2e8dccfc53f
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-20 10:04:09 -0800

    updated files 6-8 for kernel style

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * delete_dnodeint_at_index - Deletes the node at a given index.
+ * @h: Pointer to a pointer to the head of the list.
+ * @idx: Index of the node to be deleted.
+ * Return: 1 if it succeeded, -1 if it failed.
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *temp = *h;
@@ -9,7 +15,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
-
 	new_node->n = n;
 	if (idx == 0)
 	{
@@ -19,9 +24,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (*h);
 	}
-
-	for (i = 0; i < idx - 1 && temp; i++, temp = temp->next);
-
+	for (i = 0; i < idx - 1 && temp; i++)
+		temp = temp->next;
 	if (!temp)
 	{
 		free(new_node);
@@ -32,6 +36,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (temp->next)
 		temp->next->prev = new_node;
 	temp->next = new_node;
-
 	return (new_node);
 }
