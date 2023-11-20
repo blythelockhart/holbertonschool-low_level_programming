9e46a7bdc8024755e358a8d44ce6eec0516e0e17
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-20 10:00:07 -0800

    updated files 6-8 for kernel style

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -6,7 +6,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *temp = *h;
 	unsigned int i;
 
-	if (!(new_node = malloc(sizeof(dlistint_t))))
+	new_node = malloc(sizeof(dlistint_t))
+	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
@@ -15,7 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new_node->prev = NULL, new_node->next = *h;
 		if (*h)
 			(*h)->prev = new_node;
-		return *h = new_node;
+		*h = new_node;
+		return (*h);
 	}
 
 	for (i = 0; i < idx - 1 && temp; i++, temp = temp->next);
@@ -27,7 +29,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	new_node->prev = temp;
 	new_node->next = temp->next;
-	if (temp->next) temp->next->prev = new_node;
+	if (temp->next)
+		temp->next->prev = new_node;
 	temp->next = new_node;
 
 	return (new_node);
