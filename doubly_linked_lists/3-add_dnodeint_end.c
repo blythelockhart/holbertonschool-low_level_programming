a26285c031e775d1b656d728225d07c8040052b2
Blythe Lockhart <blythe.lockhart@att.net>
2023-11-20 09:38:21 -0800

    updated files 3-5 for kerel style

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,7 +25,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	{
 		new_node->prev = NULL;
 		*head = new_node;
-		return new_node;
+		return (new_node);
 	}
 
 	/* Traverse the list to find the last node */
