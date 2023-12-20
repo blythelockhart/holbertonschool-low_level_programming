8236e47b5c6691c85407841eef367e46f72a256b
Blythe Lockhart <blythe.lockhart@att.net>
2023-12-19 22:46:40 -0800

    coded files 2, 3, 4, 5, and 6

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,10 +2,13 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	// Check for NULL array
+	size_t i;
+
 	if (array == NULL)
 		return (-1);
-	for (size_t i = 0; i < size; i++) {
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -4,14 +4,22 @@ int binary_search(int *array, size_t size, int value)
 {
     int low = 0;
     int high = size - 1;
-    size_t i;
 
     if (array == NULL)
         return (-1);
     while (low <= high)
     {
         int mid = low + (high - low) / 2;
+	int i;
 
+	printf("Searching in array: ");
+        for (i = low; i <= high; i++)
+	{
+            printf("%d", array[i]);
+	    if (i < high)
+                printf(", ");
+	}
+        printf("\n");
         if (array[mid] == value)
             return mid;
         else if (array[mid] < value)
