48007246a8b760610af07e74608e376d5adeb334
Blythe Lockhart <blythe.lockhart@att.net>
2023-12-20 00:14:37 -0600

    coded files 0, 1, and search_algos

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,4 +1,13 @@
-#include <stdio.h>
+#include "search_algos.h"
 
-int linear_search(int *array, size_t size, int value) {
+int linear_search(int *array, size_t size, int value)
+{
 	// Check for NULL array
+	if (array == NULL)
+		return (-1);
+	for (size_t i = 0; i < size; i++) {
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
