c4ae053a8620786a220ebeae5a8f1ac7bd64be09
Blythe Lockhart <blythe.lockhart@att.net>
2023-12-20 00:19:05 -0600

    Updated file 1 include

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "search_algos.h"
 
 int binary_search(int *array, size_t size, int value)
 {
@@ -8,7 +8,8 @@ int binary_search(int *array, size_t size, int value)
 
     if (array == NULL)
         return (-1);
-    while (low <= high) {
+    while (low <= high)
+    {
         int mid = low + (high - low) / 2;
 
         if (array[mid] == value)
